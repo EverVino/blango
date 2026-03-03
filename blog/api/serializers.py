2842586b26ca8163958062646605fa96aa74d2ba
Ever Vino <illidan_x5@hotmail.com>
2026-03-03 01:24:07 +0000

    finish nested relationships

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,6 +1,27 @@
 from rest_framework import serializers
-from blog.models import Post, Tag
+from blog.models import Post, Tag, Comment
 from blango_auth.models import User
+
+class TagField(serializers.SlugRelatedField):
+    def to_internal_value(self, data):
+        try:
+            return self.get_queryset().get_or_create(values=data.lower())[0]
+        except (TypeError, ValueError):
+            self.fail(f"Tag values {data} is invalid")
+            
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+      model = User
+      fields = ["first_name", "last_name", "email"]
+
+class CommentSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    creator = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ["id", "creator", "content", "modified_at", "created_at"]
+        readonly = ["modified_at", "created_at"]
 
 class PostSerializer(serializers.ModelSerializer):
     tags = serializers.SlugRelatedField(
@@ -16,9 +37,22 @@ class PostSerializer(serializers.ModelSerializer):
         fields = "__all__"
         readonly = ["modified_at", "created_at"]
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-      model = User
-      fields = ["first_name", "last_name", "email"]
+class PostDetailSerializer(PostSerializer):
+    comments = CommentSerializer(many=True)
+
+    def update(self, instance, validated_data):
+        comments = validated_data.pop("comments")
+
+        instance = super(PostDetailSerializer, self).update(instance, validated_data)
+
+        for comment_dta in comments:
+            if comment_data.get("id"):
+                continue
+            comment = Comment(**comment_data)
+            comment.createor = self.context["request"].user
+            comment.content_object = instance
+            comment.save()
+
+        return instance
 
         
