75e93a178d0f26dac7b8d0078350c92a57c6ebb7
Ever Vino <illidan_x5@hotmail.com>
2026-03-10 16:43:32 +0000

    finish viewsets and routers

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -8,6 +8,11 @@ class TagField(serializers.SlugRelatedField):
             return self.get_queryset().get_or_create(values=data.lower())[0]
         except (TypeError, ValueError):
             self.fail(f"Tag values {data} is invalid")
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
             
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
